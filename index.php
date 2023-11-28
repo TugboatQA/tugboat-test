bc5492d47058ea9bebf5bb3af6fb2ff488eb930f
Zequi Vazquez <zequi@lullabot.com>
2023-11-28 15:11:22 +0100

    HTML5 video player with autoplay

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo $_SERVER['HTTP_HOST'];
+<html>
+    <head>
+        <title>Tugboat Test Page</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <video width="640" height="360" autoplay loop>
+            <source src="movie.mp4" type="video/mp4" />
+            <source src="movie.mov" type="video/mov" />
+            Your browser does not support the video tag.
+        </video>
+
+    </body>
+</html>
