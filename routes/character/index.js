ad5cd66d8d1b7ab00b798ebb82af5e1abbde6b40
James Bain <jamescbain@gmail.com>
2024-01-15 16:30:29 -0700

    handle error

--- routes/character/index.js
+++ routes/character/index.js
@@ -11,18 +11,45 @@ const template = `
   </html>
 `;
 
+const errTemplate = `
+  <html>
+    <head>
+      <title>Tugboat Test Page Error</title>
+    </head>
+    <body>
+      <h1>Tugboat Test</>
+      <p>You've made it here by mistake</p>
+    </body>
+  </html>
+`;
+
 module.exports = async function (fastify, opts) {
   fastify.get("/valid", async function (request, reply) {
-    const redirect = `${TUGBOAT_DEFAULT_SERVICE_URL}/character/hello`;
+    const redirect = `${
+      TUGBOAT_DEFAULT_SERVICE_URL || "localhost:3000"
+    }/character/hello`;
     return reply.redirect(redirect);
   });
 
   fastify.get("/invalid", async function (request, reply) {
-    const redirect = `${TUGBOAT_DEFAULT_SERVICE_URL}/character/hållo`;
+    const redirect = `${
+      TUGBOAT_DEFAULT_SERVICE_URL || "localhost:3000"
+    }/character/hållo`;
     return reply.redirect(redirect);
   });
 
   fastify.get("/hello", async function (request, reply) {
     return reply.type("text/html").send(template);
+  });
+
+  fastify.setErrorHandler(async function (err, request, reply) {
+    if (
+      err.code === 404 &&
+      err.message === "Route GET:/character/h%EF%BF%BDllo not found"
+    ) {
+      return reply.code(404).type("text/html").send(errTemplate);
+    }
+
+    throw err;
   });
 };
