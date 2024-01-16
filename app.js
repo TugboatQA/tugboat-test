ab759b627b827ea3c39d43fefdfb890c749990f9
James Bain <jamescbain@gmail.com>
2024-01-16 10:54:10 -0700

    add route that is encoded

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
+const path = require("path");
+const AutoLoad = require("@fastify/autoload");
+
+const errTemplate = `
+  <html>
+    <head>
+      <title>Tugboat Test Page Error</title>
+    </head>
+    <body>
+      <h1>Tugboat Test</>
+      <p>You've made it here by mistake</p>
+    </body>
+  </html>
+`;
 
 // Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {}
+module.exports.options = {};
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -15,14 +27,14 @@ module.exports = async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
--- routes/character/index.js
+++ routes/character/index.js
@@ -11,45 +11,22 @@ const template = `
   </html>
 `;
 
-const errTemplate = `
-  <html>
-    <head>
-      <title>Tugboat Test Page Error</title>
-    </head>
-    <body>
-      <h1>Tugboat Test</>
-      <p>You've made it here by mistake</p>
-    </body>
-  </html>
-`;
-
 module.exports = async function (fastify, opts) {
   fastify.get("/valid", async function (request, reply) {
     const redirect = `${
-      TUGBOAT_DEFAULT_SERVICE_URL || "localhost:3000"
-    }/character/hello`;
+      TUGBOAT_DEFAULT_SERVICE_URL || "http://localhost:3000"
+    }/character/h%C3%A5llo`;
     return reply.redirect(redirect);
   });
 
   fastify.get("/invalid", async function (request, reply) {
     const redirect = `${
-      TUGBOAT_DEFAULT_SERVICE_URL || "localhost:3000"
+      TUGBOAT_DEFAULT_SERVICE_URL || "http://localhost:3000"
     }/character/hållo`;
     return reply.redirect(redirect);
   });
 
-  fastify.get("/hello", async function (request, reply) {
+  fastify.get("/hållo", async function (request, reply) {
     return reply.type("text/html").send(template);
-  });
-
-  fastify.setErrorHandler(async function (err, request, reply) {
-    if (
-      err.code === 404 &&
-      err.message === "Route GET:/character/h%EF%BF%BDllo not found"
-    ) {
-      return reply.code(404).type("text/html").send(errTemplate);
-    }
-
-    throw err;
   });
 };
