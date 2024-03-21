73191291da929ee07081b14f1e51cba446c7b708
James Bain <jamescbain@gmail.com>
2024-03-21 16:44:50 -0600

    add more special character headers

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,6 +3,7 @@
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
     reply.header("bad-header", "some bad contenté");
+    reply.header("characters", "有我他这");
     return { root: true };
   });
 };
