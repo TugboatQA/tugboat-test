dce051a1d8e7262f115f2e182c085f001193f509
James Bain <jamescbain@gmail.com>
2024-03-21 16:52:52 -0600

    remove old header

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,7 +3,6 @@
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
     reply.header("bad-header", "some bad contenté");
-    reply.header("characters", "有我他这");
     return { root: true };
   });
 };
