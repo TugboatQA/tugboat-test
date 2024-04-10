bd0b0fdd57e6a773ecba379a044a13e5048c43c7
James Bain <jamescbain@gmail.com>
2024-04-10 10:51:48 -0600

    parameterize a route for waiting

--- routes/response/index.js
+++ routes/response/index.js
@@ -5,6 +5,15 @@ module.exports = async function (fastify, opts) {
     await wait(60 * 1000);
     return greeting;
   });
+
+  fastify.get("/long/:time", async function (request, reply) {
+    const greeting = "hello";
+    const { time } = request.params;
+    const multiplier = time ? time : 1;
+
+    await wait(multiplier * 60 * 1000);
+    return greeting;
+  });
 };
 
 async function wait(time) {
