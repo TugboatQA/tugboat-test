9133ae503210575ab187894fe47013f3076b233e
James Bain <jamescbain@gmail.com>
2025-02-27 15:52:02 -0700

    specify 0.0.0.0 as host

--- server/index.js
+++ server/index.js
@@ -20,6 +20,6 @@ app.get("/large-header/:size", async (req, reply) => {
 
 app.log.info(app.server);
 
-app.listen({ port: 3000 }, () => {
+app.listen({ port: 3000, host: "0.0.0.0" }, () => {
   console.log("Server is running on port 3000");
 });
