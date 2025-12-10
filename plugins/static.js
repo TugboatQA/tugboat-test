9535159e8498f9f750e951c6f9c08ee8a20ee083
James Bain <jamescbain@gmail.com>
2025-12-09 20:36:20 -0700

    fix: use path as static host

--- plugins/static.js
+++ plugins/static.js
@@ -3,9 +3,11 @@
 const fp = require("fastify-plugin");
 const path = require("path");
 
+const prefix = process.env.TUGBOAT_SERVICE_URL_PATH || "/";
+
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/static"), {
     root: path.join(__dirname, "../public"),
-    prefix: "/",
+    prefix,
   });
 });
