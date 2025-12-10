69ca463794dec03cf5fea842c80d9be1845461fa
James Bain <jamescbain@gmail.com>
2025-12-09 21:40:15 -0700

    redirect to index

--- plugins/static.js
+++ plugins/static.js
@@ -5,15 +5,8 @@ const path = require("path");
 
 module.exports = fp(async function (fastify, opts) {
   fastify.addHook("onRequest", (request, reply, done) => {
-    fastify.log.info(`request.url: ${request.url}`);
-    fastify.log.info(
-      `process.env.TUGBOAT_SERVICE_URL_PATH: ${process.env.TUGBOAT_SERVICE_URL_PATH}`
-    );
-    if (
-      process.env.TUGBOAT_SERVICE_URL_PATH &&
-      request.url === process.env.TUGBOAT_SERVICE_URL_PATH
-    ) {
-      return reply.redirect("/");
+    if (request.url === "/") {
+      return reply.redirect("/index.html");
     }
     done();
   });
