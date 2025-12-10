1ee3309ee381c874672126bdce2497c091e41fa5
James Bain <jamescbain@gmail.com>
2025-12-10 09:35:05 -0700

    redirect non-trailing slash base requests

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const baseurlExists = process.env.TUGBOAT_PREVIEW && process.env.TUGBOAT_TOKEN;
+const baseurl = baseurlExists
+  ? `/${process.env.TUGBOAT_PREVIEW}-${process.env.TUGBOAT_TOKEN}/`
+  : "/";
+
 module.exports = async function (fastify, opts) {
   fastify.register(require("./plugins/static"));
   fastify.register(require("./plugins/sensible"));
@@ -7,4 +12,13 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(require("./routes"));
+
+  // Handle not found requests
+  app.setNotFoundHandler((req, reply) => {
+    // check if the affected route is equivalent to the baseurl without a trailing slash
+    if (config.baseurlExists && req.url === baseurl.replace(/\/$/, "")) {
+      return reply.redirect(baseurl);
+    }
+    return reply.code(404).send({ error: "Not Found" });
+  });
 };
