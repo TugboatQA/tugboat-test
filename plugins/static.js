d0de79a37370a32273662c37a286ec0402de0567
James Bain <jamescbain@gmail.com>
2025-12-09 21:30:42 -0700

    epxlicitly add a redirect

--- plugins/static.js
+++ plugins/static.js
@@ -4,6 +4,15 @@ const fp = require("fastify-plugin");
 const path = require("path");
 
 module.exports = fp(async function (fastify, opts) {
+  fastify.addHook("onRequest", (request, reply, done) => {
+    if (
+      process.env.TUGBOAT_SERVICE_URL_PATH &&
+      request.url === process.env.TUGBOAT_SERVICE_URL_PATH
+    ) {
+      return reply.redirect("/");
+    }
+    done();
+  });
   fastify.register(require("@fastify/static"), {
     root: path.join(__dirname, "../public"),
     prefix: "/",
