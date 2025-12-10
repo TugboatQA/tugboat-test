2d40818d24a89e34c76a7cfb978c0cdc2c8aff6c
James Bain <jamescbain@gmail.com>
2025-12-09 20:42:26 -0700

    fix: use explicit relative to reference files

--- plugins/static.js
+++ plugins/static.js
@@ -3,11 +3,9 @@
 const fp = require("fastify-plugin");
 const path = require("path");
 
-const prefix = process.env.TUGBOAT_SERVICE_URL_PATH || "/";
-
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/static"), {
     root: path.join(__dirname, "../public"),
-    prefix,
+    prefix: "/",
   });
 });
