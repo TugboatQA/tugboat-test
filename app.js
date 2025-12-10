e59dd13bdd0cad2c261e2c16933baccb39fe19d3
James Bain <jamescbain@gmail.com>
2025-12-09 21:52:56 -0700

    attempt something crazy

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require("path");
 const AutoLoad = require("@fastify/autoload");
 
 // Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {
-  prefix: process.env.TUGBOAT_SERVICE_URL_PATH || "/",
-};
+module.exports.options = {};
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -25,6 +23,9 @@ module.exports = async function (fastify, opts) {
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, "routes"),
-    options: Object.assign({}, opts),
+    options: Object.assign(
+      {},
+      { ...opts, prefix: process.env.TUGBOAT_SERVICE_URL_PATH || "/" }
+    ),
   });
 };
