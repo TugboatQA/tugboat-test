df1a247ba1ba0abfed47d238e6ebd95f66e93596
James Bain <jamescbain@gmail.com>
2025-12-10 08:59:45 -0700

    add prefix

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 const fastify = require("fastify")({ logger: true });
 const app = require("./app");
 
+const prefix = process.env.TUGBOAT_SERVICE_URL_PATH
+  ? `${process.env.TUGBOAT_SERVICE_URL_PATH}/`
+  : "/";
+
 async function start() {
-  await fastify.register(app);
+  await fastify.register(app, { prefix });
   await fastify.listen({ port: 3000 });
   fastify.log.info(`server listening on ${fastify.server.address().port}`);
 }
