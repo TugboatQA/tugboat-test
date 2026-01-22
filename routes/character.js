82cf3369bff6b4ff23075c234231840ead038e03
James Bain <jamescbain@gmail.com>
2026-01-22 15:50:36 -0700

    add a not found error template

--- routes/character.js
+++ routes/character.js
@@ -30,4 +30,17 @@ module.exports = async function (fastify, opts) {
     reply.headers({ language: "français", greeting: "allô" });
     return reply.type("text/html").send(template);
   });
+
+  fastify.setNotFoundHandler(function (request, reply) {
+    const errorTemplate = `
+        <html>
+          <head>
+            <title>Tugboat Test Page</title>
+          </head>
+          <body>
+            <h1>Tugboat Test Error</>
+          </body>
+        </html>`;
+    return reply.type("text/html").send(errorTemplate);
+  });
 };
