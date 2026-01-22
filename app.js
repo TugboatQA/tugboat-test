00be5f273bd8c459482d0c0f962daeda79ad98fd
James Bain <jamescbain@gmail.com>
2026-01-22 16:17:53 -0700

    dynamically replace error path

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const fs = require("fs").promises;
+const path = require("path");
+
 const baseurlExists = process.env.TUGBOAT_PREVIEW && process.env.TUGBOAT_TOKEN;
 const baseurl = baseurlExists
   ? `/${process.env.TUGBOAT_PREVIEW}-${process.env.TUGBOAT_TOKEN}/`
@@ -14,14 +17,19 @@ module.exports = async function (fastify, opts) {
   fastify.register(require("./routes"));
 
   // Handle not found requests
-  fastify.setNotFoundHandler((req, reply) => {
+  fastify.setNotFoundHandler(async (req, reply) => {
     // check if the affected route is equivalent to the baseurl without a trailing slash
     if (baseurlExists && req.url === baseurl.replace(/\/$/, "")) {
       return reply.redirect(baseurl);
     }
-    return reply
-      .code(404)
-      .type("text/html")
-      .sendFile("error.html", { root: fastify.publicRoot });
+
+    const errorHtmlPath = path.join(fastify.publicRoot, "error.html");
+    let errorHtml = await fs.readFile(errorHtmlPath, "utf-8");
+    errorHtml = errorHtml.replace(
+      /<head>/,
+      `<head>\n    <base href="${baseurl}">`
+    );
+
+    return reply.code(404).type("text/html").send(errorHtml);
   });
 };
